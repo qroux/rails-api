504e3ed135df3fae955457ac7e331350fd360bc0
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-06-26 18:58:40 +0200

    booking availability fixed

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -23,4 +23,18 @@ RSpec.describe Booking, type: :model do
     Booking.create(listing_id: Listing.last.id, start_date: Date.tomorrow, end_date: Date.today)
     expect(Booking.count).to eq(0)
   end
+
+  it "validation: Booking should fail if already booked for the same period" do
+    Listing.create(num_rooms: 10)
+    Booking.create(listing_id: Listing.last.id, start_date: Date.today, end_date: Date.tomorrow)
+    Booking.create(listing_id: Listing.last.id, start_date: Date.today, end_date: Date.tomorrow)
+    expect(Booking.count).to eq(1)
+  end
+
+  it "validation: Booking should work if already booked for another period" do
+    Listing.create(num_rooms: 10)
+    Booking.create(listing_id: Listing.last.id, start_date: Date.today, end_date: Date.tomorrow)
+    Booking.create(listing_id: Listing.last.id, start_date: Date.new(2019,7,25), end_date: Date.new(2019,7,30))
+    expect(Booking.count).to eq(2)
+  end
 end
